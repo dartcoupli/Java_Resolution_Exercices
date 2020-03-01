4348c6f236bfe8ab98af2fe6365f6174229c4406
dartcoupli <51886912+dartcoupli@users.noreply.github.com>
2020-03-01 22:17:02 +0100

    numberGreaterEquals
    Introducir dos números, te dice si cual es el mayor o si son iguales.

---
+++ numberGreaterEquals.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) throws Exception
+    {
+        // Declaración de variables e inicialización.
+		int numberA;
+        int numberB;
+        
+        try 
+        {
+	       Scanner numeroInput = new Scanner(System.in);  // Create a Scanner object
+	        
+	       System.out.println("Dime el primer número: ");
+	       numberA = numeroInput.nextInt();
+	       System.out.println("Dime el segundo número: ");
+           numberB = numeroInput.nextInt();
+        
+           if (numberA < numberB)
+           {
+                System.out.println(numberB + " es el número mayor");
+           }
+           else if (numberB < numberA)
+           {
+               System.out.println(numberA + " es el número mayor");
+           }
+           else if (numberA == numberB)
+           {
+               System.out.println("Son iguales");
+           }
+        }
+        catch (Exception e)
+        {
+            
+        }
+    }
+}
